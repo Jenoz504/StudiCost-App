670ea6e6989428be9c2e6ce2a70efd55b96b36b8
Manuel <cm039398@gmail.com>
2024-03-04 19:21:57 -0600

    Navabar Terminado
    Falta el menu deslizable

---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+
+export class LoginComponent {
+
+}
+
+
